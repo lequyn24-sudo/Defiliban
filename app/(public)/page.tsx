907104961d5a60af82c8ec2f6bb92aa2c4198da6
lequyn24-sudo <lequyn24@gmail.com>
2026-06-04 14:13:40 +0700

    style: reduce sponsored and press release articles to 3

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -16,13 +16,13 @@ export default function HomePage() {
 
   const sponsoredArticles = allPreviews
     .filter((a) => a.isSponsor || a.tier1 === 'sponsored-articles')
-    .concat(allPreviews.filter((a) => a.tier1 === 'protocols').slice(0, 5))
-    .slice(0, 5)
+    .concat(allPreviews.filter((a) => a.tier1 === 'protocols').slice(0, 3))
+    .slice(0, 3)
 
   const pressReleaseArticles = allPreviews
     .filter((a) => a.tier1 === 'press-release')
-    .concat(allPreviews.filter((a) => a.tier1 === 'infrastructure').slice(0, 5))
-    .slice(0, 5)
+    .concat(allPreviews.filter((a) => a.tier1 === 'infrastructure').slice(0, 3))
+    .slice(0, 3)
 
   return (
     <div>
